b83b8e8afe5444336bb907faa894aedfe2f65d45
yxmagithub <ama.agarcorp@gmail.com>
2021-10-31 12:29:21 -0500

    Update TecnadyneMotorController.ino

--- ArduinoPrj/TecnadyneMotorController/TecnadyneMotorController.ino
+++ ArduinoPrj/TecnadyneMotorController/TecnadyneMotorController.ino
@@ -6,7 +6,13 @@
 
 #warning implmement
 
-uint8_t getBCC(uint8_t *buf, size_t len) {
+/**
+ *CalCS : calculate the Checksum for a array of bytes
+ *Input  : buf is pointer to array of bytes
+ *        : len is the length of array
+ *Output : result is the sum of the each element of array buf
+ */
+uint8_t CalCS(uint8_t *buf, size_t len) {
   uint8_t result = 0;
   for(int i = 0; i < len-1; ++i) {
     result += buf[i];
@@ -21,7 +27,7 @@ void setSpeedPWM(uint8_t address, uint8_t dir, uint8_t PWM_duty) {
   uint8_t buf[] = {0x10, 0x01, address, 0x41, 0x20, 0x20, dir, PWM_duty, 0x10, 0x03, 0x00};
 
   // Compute checksum.
-  uint8_t bcc = getBCC(buf, len);
+  uint8_t bcc = CalCS(buf, len);
   buf[len-1] = bcc;
 
   // Handle DLE stuffing.
